5f01709adbbce5651eb3bd71f7b736c6ca0f5553
Vít Brykner <johnny.brykner@gmail.com>
2023-06-17 14:02:42 +0200

    Add dusk_api scope
    For the sake of the production server.

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@
 
 module DuskAPI
   class Routes < Hanami::Routes
-    root to: "home.show"
-    get "/authenticate/:username", to: "users.authenticate"
-    # get "/users", to: "users.index"
-    scope "users" do
-      use DuskAPI::JwtMiddleware
+    scope "/dusk_api" do
+      root to: "home.show"
+      get "/authenticate/:username", to: "users.authenticate"
+      # get "/users", to: "users.index"
+      scope "users" do
+        use DuskAPI::JwtMiddleware
 
-      get "/:username", to: "users.show"
-      post "/:username/add_to/:list_name", list_name: /(to_watch|previously_watched)/, to: "users.add.index"
-      delete "/:username/remove_from/:list_name/:film_id", list_name: /(to_watch|previously_watched)/, to: "users.remove.index"
-      # put "/:username/move_to/:list_name/:film_id", list_name: /(to_watch|previously_watched)/, to: "users.move.index"
+        get "/:username", to: "users.show"
+        post "/:username/add_to/:list_name", list_name: /(to_watch|previously_watched)/, to: "users.add.index"
+        delete "/:username/remove_from/:list_name/:film_id", list_name: /(to_watch|previously_watched)/, to: "users.remove.index"
+        # put "/:username/move_to/:list_name/:film_id", list_name: /(to_watch|previously_watched)/, to: "users.move.index"
+      end
     end
   end
 end
