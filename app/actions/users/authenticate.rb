f1f8d1f6ed12bc2120b25d97e1f3ab01c8f38194
Vít Brykner <johnny.brykner@gmail.com>
2023-09-21 13:58:54 +0200

    Return authenticated user
    In order to be able to directly proceed after the auth request.

--- app/actions/users/authenticate.rb
+++ app/actions/users/authenticate.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require "jwt"
+require "oj"
 
 module DuskAPI
   module Actions
@@ -11,16 +12,16 @@ module DuskAPI
 
         def handle(request, response)
           dynamodb_service = DuskAPI::DynamodbService.new
-          does_user_exist = dynamodb_service.get_user(request.params[:username])
+          authenticated_user = dynamodb_service.get_user(request.params[:username])
 
           response.format = :json
 
-          if does_user_exist
+          if authenticated_user
             jwt_service = DuskAPI::JwtService.new
             token = jwt_service.generate_jwt_token(request.params[:username])
             response.headers["Set-Cookie"] = "account_token=#{token}; SameSite=None; Secure; HttpOnly; Partitioned"
             response.status = 200
-            response.body = {message: "Successfully authenticated"}.to_json
+            response.body = Oj.dump(authenticated_user)
           else
             response.status = 404
             response.body = {error: "User not found"}.to_json
